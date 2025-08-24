bdf5e9a87df62e8baea089ecca72d1b43c04fa4b
unknown <naeem9924066@gmail.com>
2025-08-25 03:21:54 +0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+import route from "./routes/userRoute.js"
+import cors from "cors"
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+dotenv.config()
+const PORT=process.env.PORT || 7000
+const MongoURL= process.env.MONGO_URL
+
+mongoose
+        .connect(MongoURL)
+        .then(()=>{
+            console.log("DB connected successfully")
+            app.listen(PORT,()=>{
+                console.log(`server is running on port :${PORT}`)
+            })
+        })
+        .catch((error)=> console.log)
+
+app.use("/api",route)
---
+++ server/routes/userRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { create,getAllUsers,getUserByID,updateUser,deleteData } from "../controller/userController.js";
+
+const route=express.Router();
+
+route.post("/user",create)
+route.get("/users",getAllUsers)
+route.get("/user/:id",getUserByID)
+route.put("/update/user/:id",updateUser)
+route.delete("/delete/user/:id",deleteData)
+export default  route;
